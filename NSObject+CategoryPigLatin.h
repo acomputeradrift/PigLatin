d453f6eeedc4031c4452885403c6472a0c29a5df
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-04 16:06:43 -0700

    Added notes

---
+++ NSObject+CategoryPigLatin.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+CategoryPigLatin.h
+//  PigLatin
+//
+//  Created by Jamie on 2018-08-04.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (CategoryPigLatin)
+
+-(NSString *)stringByPigLatinization;
+
+@end
---
+++ NSObject+CategoryPigLatin.m
@@ -0,0 +1,22 @@
+//
+//  NSObject+CategoryPigLatin.m
+//  PigLatin
+//
+//  Created by Jamie on 2018-08-04.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import "NSObject+CategoryPigLatin.h"
+
+@implementation NSObject (CategoryPigLatin)
+
+-(NSString *)stringByPigLatinization{
+    //break string into words using " "
+    //break each word into char
+    //grab the first char of each word and add it to the end with ay append
+    //print string
+    return pigString;
+}
+
+@end
+
